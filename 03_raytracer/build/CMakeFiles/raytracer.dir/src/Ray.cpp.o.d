48cee38f51d24350337db0a1350f8cca3595bcb9
TimothyPav <1000timka@gmail.com>
2024-10-18 11:34:26 -0500

    added all gitignores

--- 03_raytracer/build/CMakeFiles/raytracer.dir/src/Ray.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/raytracer.dir/src/Ray.cpp.o: \
- /home/tim/projects/fifty/03_raytracer/src/Ray.cpp \
- /usr/include/stdc-predef.h \
- /home/tim/projects/fifty/03_raytracer/include/Ray.hpp \
- /home/tim/projects/fifty/03_raytracer/include/Vector3D.hpp
