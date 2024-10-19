f41248254c8b717d8ee76cc1db81b6a404ff64c5
TimothyPav <1000timka@gmail.com>
2024-10-19 09:59:49 -0500

    added progress bar with volume slider

--- 05_music_player/src/main/java/com/timothypav/musicplayer/MusicPlayerApp.java
+++ 05_music_player/src/main/java/com/timothypav/musicplayer/MusicPlayerApp.java
@@ -5,6 +5,7 @@ import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -36,10 +37,11 @@ public class MusicPlayerApp extends Application {
         s.setMin(0);
         s.setMax(.5);
         s.setValue(.05);
-        s.setOrientation(Orientation.VERTICAL);
+        s.setOrientation(Orientation.HORIZONTAL);
         s.setUnitIncrement(.001);
         s.setBlockIncrement(.001);
         mediaPlayer.setVolume(s.getValue());
+
 
         // Create a Label to display the value
         Label valueLabel = new Label("Current Value: " + s.getValue());
@@ -53,8 +55,18 @@ public class MusicPlayerApp extends Application {
             mediaPlayer.setVolume(value);
         });
 
+        ProgressBar bar = new ProgressBar(0);
+
+        mediaPlayer.currentTimeProperty().addListener((observable,  oldValue, newValue) -> {
+            double value = newValue.toMinutes();
+            double total_duration = mediaPlayer.getTotalDuration().toMinutes();
+            double progress = value/total_duration;
+            bar.setProgress(progress);
+        });
+
+
         // Layout with buttons
-        VBox root = new VBox(stopButton, playButton, s, valueLabel);
+        VBox root = new VBox(stopButton, playButton, s, valueLabel, bar);
 
         Scene scene = new Scene(root, 300, 500);
         stage.setTitle("Music Player");
